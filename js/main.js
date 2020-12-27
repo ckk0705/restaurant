535986dba99ba93ead5d8ab00ea62cc8a86bb861
ckk0705 <75234929+ckk0705@users.noreply.github.com>
2020-12-27 16:19:16 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
+import * as App from './app.js';
 
+(function() {
+
+  function ready() {
+      
+    return new Promise(function(resolve, reject) {
+      
+      // resolve the promise when the document is ready
+      document.addEventListener('readystatechange', function() {
+        if(document.readyState !== 'loading') {
+          resolve();
+        }
+      });
+
+    });
+
+  };
+
+  ready().then(function() {
+    App.init();
+
+    // add filtering listeners
+    var typeButtons = document.getElementsByName('type');
+    for(var i=0; i < typeButtons.length; i++) {
+      typeButtons[i].addEventListener('change', function(){
+
+        App.filterByType(this.value);
+
+      });
+    }
+
+    // Add input listener for the search box, we want to update
+    // the list on each keystroke (the list is already completely 
+    // loaded so this doesn't make more requests)
+    var nameFilter = document.getElementById('searchbox');
+    nameFilter.addEventListener('input', function() {
+
+      App.filterByName(this.value);
+
+    });
+
+  });
+
+})();
