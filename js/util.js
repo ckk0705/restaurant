2863e72c900473a2f9bfef3bba3c3622544665e0
ckk0705 <75234929+ckk0705@users.noreply.github.com>
2020-12-27 16:21:03 +0800

    Add files via upload

--- js/util.js
+++ js/util.js
@@ -1,1 +1,27 @@
 
+export function htmlToElement(html) {
+    var template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
+/*
+  Selects the browser transition event, from:
+  https://davidwalsh.name/css-animation-callback
+*/
+export function whichTransitionEvent(){
+    var t;
+    var el = document.createElement('fakeelement');
+    var transitions = {
+      'transition':'transitionend',
+      'OTransition':'oTransitionEnd',
+      'MozTransition':'transitionend',
+      'WebkitTransition':'webkitTransitionEnd'
+    }
+
+    for(t in transitions){
+        if( el.style[t] !== undefined ){
+            return transitions[t];
+        }
+    }
+}
